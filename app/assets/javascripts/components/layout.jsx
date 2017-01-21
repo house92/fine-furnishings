d23ae40192efdc063d289dbaf69192aec5b20904
Sam <samuel.house@ntlworld.com>
2017-01-21 14:58:22 +0000

    completed devise setup; about to redo form according to guide

--- app/assets/javascripts/components/layout.jsx
+++ app/assets/javascripts/components/layout.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Router } from 'react-router';
 import Header from './header.jsx';
 
 export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { signedIn: null };
+  }
+
+  componentWillMount() {
+    $.ajax({
+      method: "GET",
+      url: "/auth/is_signed_in.json"
+    })
+    .done(function(data){
+      this.setState({ signedIn: data.signed_in });
+    }.bind(this));
+  }
+
   render() {
     return (
       <div>
--- app/assets/javascripts/components/register.jsx
+++ app/assets/javascripts/components/register.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import { Row, Col, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import _ from 'lodash';
 import Layout from './layout.jsx';
+import Functions from '../../../lib/functions.js';
 
 export default class Register extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      name: ''
+    };
     this.passwordLengthCheck = this.passwordLengthCheck.bind(this);
+    this._handleInputChange = this._handleInputChange.bind(this);
+    this._handleRegistrationClick = this._handleRegistrationClick.bind(this);
+  }
+
+  _handleInputChange(ev) {
+    // Get a deep clone of the component's state before the input change.
+    var nextState = _.cloneDeep(this.state);
+
+    //Update the state of the component
+    nextState[ev.target.name] = ev.target.value;
+
+    // Update the component's state with the new state
+    this.setState(nextState);
+  }
+
+  _handleRegistrationClick(e) {
+    $.ajax({
+      method: "POST",
+      url: "/users.json",
+      data: {
+        user: {
+          email: this.state.email,
+          uid: this.state.email,
+          password: this.state.password,
+          password_confirmation: this.state.password_confirmation,
+          name: this.state.name,
+          provider: "email"
+        },
+        authenticity_token: Functions.getMetaContent("csrf-token")
+      }
+    })
+    .done(function(data){
+      location.reload();
+    }.bind(this));
   }
 
   passwordLengthCheck(e) {
@@ -21,19 +63,19 @@ export default class Register extends Component {
       <Layout currentUser={null}>
         <span>{deviseErrorMessages}</span>
         <form action="/users" method="POST">
-          <FormControl type="hidden" name="authenticity_token" value={authenticityToken} />
+          <FormControl type="hidden" name="authenticity_token" />
           <div class="field">
             <ControlLabel htmlFor="user_email">E-mail:</ControlLabel>
             <FormControl type="email" name="user_email" autoFocus={true} />
           </div>
-
+          
           <div class="field">
             <ControlLabel htmlFor="user_password" name="user[password]">Password:</ControlLabel>
             <FormControl type="password" onChange={this.passwordLengthCheck} autoComplete="off" />
           </div>
 
           <div class="field">
-            <ControlLabel htmlFor="user_password_confirmation" name="user[password_confirmation]">Password:</ControlLabel>
+            <ControlLabel htmlFor="user_password_confirmation" name="user[password_confirmation]">Confirm password:</ControlLabel>
             <FormControl type="password" onChange={this.passwordLengthCheck} autoComplete="off" />
           </div>
 
